43bad8406ea0384e5664eb45b791a829e7108530
zhanxw <zhanxw@gmail.com>
2011-11-23 15:32:03 -0500

    add files

---
+++ libVcf/PeopleSet.cpp
@@ -0,0 +1,48 @@
+#include "PeopleSet.h"
+#include "IO.h"
+
+// class PeopleSet
+void PeopleSet::readID(const char* allPeopleID){
+    if (!strlen(allPeopleID)) return;
+
+    std::vector<std::string> sa;
+    stringTokenize(allPeopleID, ",", &sa);
+    for (int i = 0; i< sa.size(); i++){
+        this->people.insert(sa[i]);
+    }
+}
+
+void PeopleSet::readID(const std::vector<std::string> peopleVec){
+    for (unsigned int i = 0; i < peopleVec.size(); i++ ){
+        this->people.insert(peopleVec[i]);        
+    }
+};
+
+void PeopleSet::readIDfromFile(const char* fileName) {
+    if (!strlen(fileName)) return;
+
+    std::vector <std::string> id;
+    this->obtainIDfromFile(fileName, &id);
+    for (unsigned int i = 0; i < id.size(); i++) {
+        this->readID(id[i].c_str());
+    }
+}
+
+/**
+ * File content should be:
+ * ID1
+ * ID2
+ * ID3
+ * ...
+ * (only the first column will be used)
+ */
+int  PeopleSet::obtainIDfromFile(const char* fileName, std::vector<std::string>* allID) {
+    assert(allID);
+    allID->clear();
+    std::vector<std::string> fd;
+    LineReader lr(fileName);
+    while (lr.readLineBySep(&fd, " \t")) {
+        (*allID).push_back( fd[0].c_str());
+    }
+    return 0;
+}
---
+++ libVcf/VCFFunction.h
@@ -0,0 +1,89 @@
+#ifndef _VCFFUNCTION_H_
+#define _VCFFUNCTION_H_
+
+#include "VCFValue.h"
+
+static int parseTillChar(const char c, const char* line, const int beg, VCFValue* ret) {
+    assert(ret);
+    ret->line = line;
+    ret->beg = beg;
+    ret->end = ret->beg;
+    while(line[ret->end] != c && line[ret->end] != '\0') {
+        ret->end++;
+    }
+    return ret->end;
+};
+
+static int parseTillChar(const char* c, const char* line, const int beg, VCFValue* ret) {
+    assert(ret);
+    ret->line = line;
+    ret->beg = beg;
+    ret->end = ret->beg;
+    int cLen = strlen(c);
+    while(!index(c, line[ret->end])){ // line[beg] is not a separator
+        ret->end++;
+    }
+    return ret->end;
+};
+
+/**
+ * @return 0: found @param id in @paraminfo, and store results in @param result
+ * @return -1: not found, @param result value is undetermined
+ */
+static inline int parseVCFINFO(std::string* result, const std::string info, const char* id) {
+    size_t begin = info.find(id);
+    if (begin == std::string::npos) {
+        return -1;
+    }
+    result->clear();
+    assert(info[++begin] == '=');
+    size_t len = info.size();
+    while (begin < len && info[begin] != ';')
+        (*result).push_back(info[begin++]);
+}
+
+/**
+ * @return -1: we cannot find depth
+ */
+static int obtainDepth(const char* s, int start, int end) {
+    const char* DEPTH_STRING = "DP";
+    const int dpStrLen = strlen(DEPTH_STRING);
+    bool isMatch = true;
+    int dpBegin, dpEnd;
+    for (int i = start; i < end; i++) {
+        if (s[i] != DEPTH_STRING[0])
+            continue;
+        for (int j = 1; j < dpStrLen; j++) {
+            if (s[i+j] != DEPTH_STRING[j]) {
+                isMatch = false;
+                break;
+            }
+        }
+        if (isMatch == false) 
+            continue;
+        else {
+            isMatch = true;
+            dpBegin = i;
+            break;
+        }
+    }
+    if (!isMatch) 
+        return -1;
+    
+    // move to the first digit
+    dpBegin += dpStrLen; 
+    assert(s[dpBegin] == '=');
+    dpBegin++;
+
+    // put number in buffer and then convert
+    static char buffer[32]; // handle 32 digits integer at most
+    int i = 0;
+    while(s[dpBegin] >= '0' && s[dpBegin] <= '9' && dpBegin < end) {
+        buffer[i++] = s[dpBegin++];
+    }
+    buffer[i] = '\0';
+    return atoi(buffer);
+};
+
+
+#endif /* _VCFFUNCTION_H_ */

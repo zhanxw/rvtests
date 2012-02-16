43066a3839da08323126031558f52137ba42d9de
zhanxw <zhanxw@gmail.com>
2012-02-16 16:03:12 -0500

    add libVcf automated test

---
+++ libVcf/test/testVCFExtract.cpp
@@ -0,0 +1,16 @@
+#include "VCFUtil.h"
+#include "VCFExtractor.h"
+
+int main(){
+    VCFExtractor ve;
+    ve.open("test.vcf");
+    ve.setSiteFreqMin(0.01);
+    ve.extract();
+    const std::vector<double>& af = ve.getAF();
+    for (int i = 0; i < af.size(); i++) {
+        fprintf(stdout, "i= %d, AF= %.5f\n", i, af[i]);
+    }
+    
+    ve.close();
+    return 0;
+};

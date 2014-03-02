467cd3d0eebbe12a36ffca846076eba0415d700e
zhanxw <zhanxw@gmail.com>
2014-03-02 12:31:55 -0500

    temporary

--- src/ModelFitter.h
+++ src/ModelFitter.h
@@ -9,8 +9,10 @@
 #include "base/ParRegion.h"
 #include "regression/LogisticRegression.h"
 #include "regression/LogisticRegressionScoreTest.h"
+#include "regression/LogisticRegressionVT.h"
 #include "regression/LinearRegression.h"
 #include "regression/LinearRegressionScoreTest.h"
+#include "regression/LinearRegressionVT.h"
 #include "regression/Skat.h"
 #include "regression/Table2by2.h"
 #include "regression/kbac_interface.h"
@@ -36,11 +38,12 @@ double getMarkerFrequency(Matrix& in, int col);
 double getMarkerFrequencyFromControl(Matrix& in, Vector& pheno, int col);
 
 void cmcCollapse(Matrix& in, Matrix* out);
+void cmcCollapse(Matrix& in, const std::vector<int>& idx, Matrix* out);
 void zegginiCollapse(Matrix& in, Matrix* out);
+void zegginiCollapse(Matrix& in, const std::vector<int>& idx, Matrix* out);
 void fpCollapse(Matrix& in, Matrix* out);
 void madsonBrowningCollapse(Matrix& genotype, Vector& phenotype, Matrix* out);
-
-void rearrangeGenotypeByFrequency(Matrix& in, Matrix* out, std::vector<double>* freq);
+void groupFrequency(std::vector<double> freq, std::map<double, std::vector<int> >* group);
 
 // take X, Y, Cov and fit model
 // note, ModelFitter will use VCFData as READ-ONLY data structure,
@@ -1429,8 +1432,25 @@ class VariableThresholdPrice: public ModelFitter{
       fprintf(stderr, "Price's Variable Threshold method does not take covariate.\n");
     }
 
-    rearrangeGenotypeByFrequency(genotype, &sortedGenotype, &this->freq);
-    convertToReferenceAlleleCount(&sortedGenotype);
+    // rearrangeGenotypeByFrequency(genotype, &sortedGenotype, &this->freq);
+    convertToReferenceAlleleCount(genotype, &geno);
+    freq.clear();
+    for (int i = 0; i < genotype.rows; ++i){
+      freq.push_back(getMarkerFrequency(geno, i));
+    }
+    groupFrequency(freq, &freqGroup);
+
+    sortedGenotype.Dimension(geno.rows, freqGroup.size());
+    int i = 0;
+    freq.clear();
+    for(freqGroupIter = freqGroup.begin();
+        freqGroupIter != freqGroup.end();
+        freqGroupIter ++) {
+      cmcCollapse(geno, freqGroupIter->second, &sortedGenotype, i);
+      freq.push_back(freqGroupIter->first);
+      ++i;
+    }
+    
     collapseGenotype(&sortedGenotype);
     transpose(&sortedGenotype); // now each row is a collapsed genoype at certain frequency cutoff
     copyPhenotype(phenotype, &this->phenotype);
@@ -1517,11 +1537,12 @@ class VariableThresholdPrice: public ModelFitter{
    * Convert genotype back to reference allele count
    * e.g. genotype 2 means homAlt/homAlt, so it has reference allele count 0
    */
-  void convertToReferenceAlleleCount(Matrix* g){
+  void convertToReferenceAlleleCount(Matrix& in, Matrix* g){
     Matrix& m = *g;
+    m.Dimension(in.rows, in.cols);
     for (int i = 0; i < m.rows; ++i) {
       for (int j = 0; j < m.cols; ++j) {
-        m[i][j] = 2 - m[i][j];
+        m[i][j] = 2 - in[i][j];
       }
     }
   };
@@ -1530,7 +1551,7 @@ class VariableThresholdPrice: public ModelFitter{
    * we will collpase left to right, column by column
    * it is mimic the behavior of setting different frequency cutoff
    */
-  void collapseGenotype(Matrix* g){
+  void sequentialCollapseGenotype(Matrix* g){
     Matrix& m = *g;
     for (int i = 0; i < m.rows; ++i) {
       for (int j = 1; j < m.cols; ++j) {
@@ -1572,8 +1593,11 @@ class VariableThresholdPrice: public ModelFitter{
     return ret;
   };
  private:
-  Matrix sortedGenotype;
+  Matrix geno;
+  // Matrix sortedGenotype;
   std::vector<double> freq;
+  std::map <double, std::vector<int> > freqGroup;
+      
   bool fitOK;
   Vector phenotype;
   double zmax;
@@ -1662,6 +1686,81 @@ class VariableThresholdCMC: public ModelFitter{
   // Result result;
 }; // VariableThresholdCMCTest
 
+class VTCMC {
+ public:
+  VT() {
+    this->modelName = "VTCMC";
+  };
+  int fit(DataConsolidator* dc) {
+    Matrix& phenotype = dc-> getPhenotype();
+    Matrix& genotype = dc->getGenotype();
+    Matrix& covariate= dc->getCovariate();
+    // Vector& weight = dc->getWeight();
+
+    if (genotype.cols == 0) {
+      fitOK = false;
+      return -1;
+    }
+    // copyPhenotype(phenotype, &this->phenotype);
+    int ret = fitNullModel(dc);
+    if (ret) {
+      return -1;
+    }
+    
+    rearrangeGenotypeByFrequency(genotype, &sortedGenotype, &this->freq);
+
+    if (!binaryOutcome()) {
+      ret = linear.Fit(cov, phenotype, genotype);
+    } else{
+      ret = logistic.Fit(cov, phenotype, genotype);
+    }
+    if (ret) {
+      fitOK = false;
+      return -1;
+    }
+    fitOK = true;
+    return 0;
+  }
+  // write result header
+  void writeHeader(FileWriter* fp, const Result& siteInfo) {
+    siteInfo.writeHeaderTab(fp);
+    result.addHeader("OptFreq");
+    result.writeHeaderLine(fp);
+  };
+  // write model output
+  void writeOutput(FileWriter* fp, const Result& siteInfo) {
+    siteInfo.writeValueTab(fp);
+    result.updateValue("OptFreq", 1.0);
+    result.writeValueLine(fp);
+  };
+  void reset() {
+    fitOK = true;
+  };
+  
+ private:
+  int fitNullModel(DataConsolidator* dc) {
+    Matrix& phenotype = dc-> getPhenotype();
+    Matrix& genotype = dc->getGenotype();
+    Matrix& covariate= dc->getCovariate();
+    if (this->needToFitNullModelForAuto || dc->isPhenotypeUpdated() || dc->isCovariateUpdated()) {
+      copyCovariateAndIntercept(genotype.rows, covariate, &cov);
+      copyPhenotype(phenotype, &this->pheno);
+      if (binaryOutcome()) {
+        fitOK = logistic.FitNullModel(cov, pheno);
+      } else {
+        fitOK = linear.FitNullModel(cov, pheno);        
+      }
+      if (!fitOK) return -1;
+      needToFitNullModelForAuto = false;
+    }
+
+  }
+  
+  LogisticRegressionVT logistic;
+  LinearRegressionVT linear;
+  bool fitOK;
+  bool needToFitNullModel;
+};
 #if 0
 class VariableThresholdFreqTest: public ModelFitter{
  public:
@@ -3500,276 +3599,5 @@ class DumpModel: public ModelFitter{
   std::string header;
   std::vector<std::string> rowLabel;
 }; // end DumpModel
-
-//////////////////////////////////////////////////////////////////////
-// Implementation of various collpasing methods
-/**
- * @return Madson-Browning definition of alleleFrequency
- */
-double getMarkerFrequency(Matrix& in, int col){
-  int& numPeople = in.rows;
-  double ac = 0; // NOTE: here genotype may be imputed, thus not integer
-  int an = 0;
-  for (int p = 0; p < numPeople; p++) {
-    if ( (int) in[p][col] >= 0) {
-      ac += in[p][col];
-      an += 2;
-    }
-  }
-  if ( an == 0 ) return 0.0;
-  //double freq = 1.0 * (ac + 1) / (an + 1);
-  double freq = ac / an;
-  return freq;
-};
-
-double getMarkerFrequencyFromControl(Matrix& in, Vector& pheno, int col){
-  int& numPeople = in.rows;
-  double ac = 0; // NOTE: here genotype may be imputed, thus not integer
-  int an = 0;
-  for (int p = 0; p < numPeople; p++) {
-    if (pheno[p] == 1) continue;
-    if (in[p][col] >= 0) {
-      ac += in[p][col];
-      an += 2;
-    }
-  }
-  // Refer:
-  // 1. Madsen BE, Browning SR. A Groupwise Association Test for Rare Mutations Using a Weighted Sum Statistic. PLoS Genet. 2009;5(2):e1000384. Available at: http://dx.doi.org/10.1371/journal.pgen.1000384 [Accessed November 24, 2010].
-  double freq = 1.0 * (ac + 1) / (an + 2);
-  return freq;
-};
-
-void cmcCollapse(Matrix& in, Matrix* out){
-  assert(out);
-  int numPeople = in.rows;
-  int numMarker = in.cols;
-
-  out->Dimension(numPeople, 1);
-  out->Zero();
-  for (int p = 0; p < numPeople; p++){
-    for (int m = 0; m < numMarker; m++) {
-      int g = (int)(in[p][m]);
-      if (g > 0) {
-        (*out)[p][0] = 1.0;
-        break;
-      }
-    };
-  };
-};
-
-void zegginiCollapse(Matrix& in, Matrix* out){
-  assert(out);
-  int numPeople = in.rows;
-  int numMarker = in.cols;
-
-  out->Dimension(numPeople, 1);
-  out->Zero();
-  for (int p = 0; p < numPeople; p++){
-    for (int m = 0; m < numMarker; m++) {
-      int g = (int)(in[p][m]);
-      if (g > 0) { // genotype is non-reference
-        (*out)[p][0] += 1.0;
-      }
-    };
-  };
-};
-
-/**
- * @param genotype : people by marker matrix
- * @param phenotype: binary trait (0 or 1)
- * @param out: collapsed genotype
- */
-void madsonBrowningCollapse(Matrix& genotype, Vector& phenotype, Matrix* out){
-  assert(out);
-  int& numPeople = genotype.rows;
-  int numMarker = genotype.cols;
-
-  out->Dimension(numPeople, 1);
-  out->Zero();
-
-  for (int m = 0; m < numMarker; m++) {
-    // calculate weight
-    double freq = getMarkerFrequencyFromControl(genotype, phenotype, m);
-    if (freq <= 0.0 || freq >= 1.0) continue; // avoid freq == 1.0
-    double weight = 1.0 / sqrt(freq * (1.0-freq)*genotype.rows);
-    // fprintf(stderr, "freq = %f\n", freq);
-
-    for (int p = 0; p < numPeople; p++) {
-      (*out)[p][0] += genotype[p][m] * weight;
-    }
-  };
-};
-
-void fpCollapse(Matrix& in, Matrix* out){
-  assert(out);
-  int& numPeople = in.rows;
-  int numMarker = in.cols;
-
-  out->Dimension(numPeople, 1);
-  out->Zero();
-
-  for (int m = 0; m < numMarker; m++) {
-    // calculate weight
-    double freq = getMarkerFrequency(in, m);
-    if (freq <= 0.0 || freq >= 1.0) continue; // avoid freq == 1.0
-    double weight = 1.0 / sqrt(freq * (1.0-freq));
-    // fprintf(stderr, "freq = %f\n", freq);
-
-    for (int p = 0; p < numPeople; p++) {
-      (*out)[p][0] += in[p][m] * weight;
-    }
-  };
-};
-
-void madsonBrowningCollapse(Matrix* d, Matrix* out){
-  assert(out);
-  Matrix& in = (*d);
-  int& numPeople = in.rows;
-  int numMarker = in.cols;
-
-  out->Dimension(numPeople, 1);
-  out->Zero();
-
-
-  for (int m = 0; m < numMarker; m++) {
-    // calculate weight
-    double freq = getMarkerFrequency(in, m);
-    if (freq <= 0.0 || freq >= 1.0) continue; // avoid freq == 1.0
-    double weight = 1.0 / sqrt(freq * (1.0-freq));
-    // fprintf(stderr, "freq = %f\n", freq);
-
-    for (int p = 0; p < numPeople; p++) {
-      (*out)[p][0] += in[p][m] * weight;
-    }
-  };
-};
-
-/**
- * Collapsing @param d to @param out, the order of columns in @param out is the same as @param freq
- * which is the frequency upper bounds, and @param freq will be increase frequency.
- * e.g.
- * @param in P by 3 matrix, then @param out will be 3 columns too
- * if @param freq = (0.1, 0.2, 0.3) meaning
- * @param out column 0 using 0.1 frequency upper bound, and the smallest frequency of marker is 0.1 (@param freq[0])
- * @param out column 1 using 0.2 frequency upper bound, and the second largest frequency of marker is 0.2 (@param freq[1])
- * @param out column 2 using 0.3 frequency upper bound, and the largest frequency of marker is 0.3 (@param freq[2])
- */
-void rearrangeGenotypeByFrequency(Matrix& in, Matrix* out, std::vector<double>* freq) {
-  assert(out && freq);
-  out->Dimension(in.rows, in.cols);
-
-  const int& numPeople = in.rows;
-  const int& numMarker = in.cols;
-  freq->resize(numMarker);
-
-  /* out->Dimension(numPeople, numMarker); */
-  /* if (col < 0) { */
-  /*   out->Zero(); */
-  /*   return; */
-  /* } */
-
-  for (int m = 0; m < numMarker; ++m){
-    (*freq)[m] = getMarkerFrequency(in, m);
-  }
-
-  std::vector<int> ord;
-  order(*freq, &ord);
-  std::sort(freq->begin(), freq->end());
-
-  for (int m = 0; m < numMarker; ++m){
-    const int& col = ord[m];
-    for (int p = 0; p < numPeople; ++p) {
-      (*out)[p][m] = in[p][col];
-    }
-  }
-};
-
-void progressiveCMCCollapse(Matrix* d, Matrix* out, std::vector<double>* freq) {
-  assert(out && freq);
-  Matrix& in = (*d);
-  out->Dimension(in.rows, in.cols);
-
-  const int& numPeople = in.rows;
-  const int& numMarker = in.cols;
-  freq->resize(numMarker);
-
-  /* out->Dimension(numPeople, numMarker); */
-  /* if (col < 0) { */
-  /*   out->Zero(); */
-  /*   return; */
-  /* } */
-
-  for (int m = 0; m < numMarker; ++m){
-    (*freq)[m] = getMarkerFrequency(in, m);
-  }
-
-  std::vector<int> ord;
-  order(*freq, &ord);
-  std::sort(freq->begin(), freq->end());
-
-  for (int m = 0; m < numMarker; ++m){
-    const int& col = ord[m];
-    if (m == 0) {
-      for (int p = 0; p < numPeople; ++p) {
-        if (in[p][col] > 0) {
-          (*out)[p][m] = 1;
-        }
-      }
-    } else { //
-      for (int p = 0; p < numPeople; ++p) {
-        if ((*out)[p][m-1] > 0) {
-          (*out)[p][m] = 1;
-          continue;
-        };
-        if (in[p][col] > 0) {
-          (*out)[p][m] = 1;
-          continue;
-        }
-      }
-    }
-  }
-
-};
-
-void progressiveMadsonBrowningCollapse(Matrix* d, Matrix* out, std::vector<double>* freq) {
-  assert(out);
-  Matrix& in = (*d);
-  out->Dimension(in.rows, in.cols);
-
-  int numPeople = in.rows;
-  int numMarker = in.cols;
-  freq->resize(numMarker);
-
-  /* out->Dimension(numPeople, 1); */
-  /* if (col < 0) { */
-  /*   out->Zero(); */
-  /*   return; */
-  /* } */
-
-  for (int m = 0; m < numMarker; ++m){
-    (*freq)[m] = getMarkerFrequency(in, m);
-  }
-
-  std::vector<int> ord;
-  order(*freq, &ord);
-  std::sort(freq->begin(), freq->end());
-
-  double weight;
-  for (int m = 0; m < numMarker; ++m){
-    const int& col = ord[m];
-    weight = 1.0 / sqrt (  (*freq)[m] * (1.0 - (*freq)[m])) ;
-
-    if (m == 0) {
-      for (int p = 0; p < numPeople; ++p) {
-        (*out)[p][m] = weight * in[p][col];
-      }
-    } else { //
-      for (int p = 0; p < numPeople; ++p) {
-        (*out)[p][m] = (*out)[p][m-1] + weight * in[p][col];
-      }
-    }
-  }
-};
-
 
 #endif /* _MODELFITTER_H_ */

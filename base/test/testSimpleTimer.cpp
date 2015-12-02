3a79f41b8731234e89d9512ca3cb22a594833578
zhanxw <zhanxw@gmail.com>
2015-12-02 12:44:04 -0500

    revise test

--- base/test/testSimpleTimer.cpp
+++ base/test/testSimpleTimer.cpp
@@ -1,34 +1,38 @@
+#define _USE_CXX11 // use C++11 timer
 #include "SimpleTimer.h"
 #include <stdio.h>
 #include <cassert>
+
+int takeTime() {
+    int a = 1;
+    for (int i = 1; i < 10000; i++) {
+      for (int j = 1; j < 10000; j++) {
+        a *= i;
+        a *= j;
+        a /= j;
+        a /= i;
+      }
+    }
+    return a;
+}
 
 int main(int argc, char *argv[])
 {
   {
     SimpleTimer m;
-    int a = 1;
-    for (int i = 1; i < 10000; i++) {
-      for (int j = 1; j < 10000; j++) {
-        a *= i;
-        a *= j;
-        a /= j;
-        a /= i;
-      }
-    }
+    int a = takeTime();
     fprintf(stderr, "elapsed %g seconds, and a = %d\n", m.stop(), a);
+    m.start();
+    a = takeTime();
+    fprintf(stderr, "elapsed %g seconds, and a = %d\n", m.stop(), a);    
   }
   {
     AccurateTimer m;
-    int a = 1;
-    for (int i = 1; i < 10000; i++) {
-      for (int j = 1; j < 10000; j++) {
-        a *= i;
-        a *= j;
-        a /= j;
-        a /= i;
-      }
-    }
+    int a = takeTime();
     fprintf(stderr, "elapsed %g seconds, and a = %d\n", m.stop(), a);
+    m.start();
+    a = takeTime();
+    fprintf(stderr, "elapsed %g seconds, and a = %d\n", m.stop(), a);    
   }
 
   return 0;

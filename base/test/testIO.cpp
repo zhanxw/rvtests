2d767f8b0a1599dec2f6891f999f0c23a94d27ea
zhanxw <zhanxw@gmail.com>
2015-12-02 12:44:17 -0500

    format code

--- base/test/testIO.cpp
+++ base/test/testIO.cpp
@@ -10,95 +10,93 @@
 using std::vector;
 using std::string;
 
-char a[]="abc\ndef\nHAH!!DFDSLIJFDSO\nadfa";
+char a[] = "abc\ndef\nHAH!!DFDSLIJFDSO\nadfa";
 char buffer[1024];
 
-int main(int argc, char *argv[])
-{
-    vector<string> t;
-    int ret = stringTokenize(a, '\n', &t);
-    assert (ret == 4);
+int main(int argc, char *argv[]) {
+  vector<string> t;
+  int ret = stringTokenize(a, '\n', &t);
+  assert(ret == 4);
 
-    {
-        char fn[] = "abc.txt";
-        FileWriter fw(fn);
-        fw.write(a);
-        fw.close();
-        
-        LineReader lr(fn);
-        std::string ln;
-        int i = 0;
-        while(lr.readLine(&ln)){
-            assert( 0 == strcmp ( t[i].c_str(), ln.c_str()) );
-            i ++;
-        }
-    }
+  {
+    char fn[] = "abc.txt";
+    FileWriter fw(fn);
+    fw.write(a);
+    fw.close();
 
-    {
-        char fn[] = "abc.txt.gz";
-        FileWriter fw(fn);
-        fw.write(a);
-        fw.close();
-        
-        LineReader lr(fn);
-        std::string ln;
-        int i = 0;
-        while(lr.readLine(&ln)){
-            assert( 0 == strcmp ( t[i].c_str(), ln.c_str()) );
-            i ++;
-        }
+    LineReader lr(fn);
+    std::string ln;
+    int i = 0;
+    while (lr.readLine(&ln)) {
+      assert(0 == strcmp(t[i].c_str(), ln.c_str()));
+      i++;
     }
+  }
 
-    {
-        char fn[] = "abc.txt.bz2";
-        FileWriter fw(fn);
-        fw.write(a);
-        fw.close();
-        
-        LineReader lr(fn);
-        std::string ln;
-        int i = 0;
-        while(lr.readLine(&ln)){
-            assert( 0 == strcmp ( t[i].c_str(), ln.c_str()) );
-            i ++;
-        }
-    }
+  {
+    char fn[] = "abc.txt.gz";
+    FileWriter fw(fn);
+    fw.write(a);
+    fw.close();
 
-    {
-        char fn[] = "abc.txt.bgzip.gz";
-        FileWriter fw(fn, BGZIP);
-        fw.write(a);
-        fw.close();
-        
-        LineReader lr(fn);
-        std::string ln;
-        int i = 0;
-        while(lr.readLine(&ln)){
-            assert( 0 == strcmp ( t[i].c_str(), ln.c_str()) );
-            i ++;
-        }
+    LineReader lr(fn);
+    std::string ln;
+    int i = 0;
+    while (lr.readLine(&ln)) {
+      assert(0 == strcmp(t[i].c_str(), ln.c_str()));
+      i++;
+    }
+  }
 
-    }
+  {
+    char fn[] = "abc.txt.bz2";
+    FileWriter fw(fn);
+    fw.write(a);
+    fw.close();
 
-    {
-      char fn[] = "http://zhanxw.com/rvtests/version";
-      LineReader lr(fn);
-      std::string ln;
-        int i = 0;
-        while(lr.readLine(&ln)){
-          fprintf(stderr, "line %d: %s\n", i + 1, ln.c_str());
-            i ++;
-        }
+    LineReader lr(fn);
+    std::string ln;
+    int i = 0;
+    while (lr.readLine(&ln)) {
+      assert(0 == strcmp(t[i].c_str(), ln.c_str()));
+      i++;
     }
-    {
-      char fn[] = "http://zhanxw.com/rvtests/version.gz";
-      LineReader lr(fn);
-      std::string ln;
-        int i = 0;
-        while(lr.readLine(&ln)){
-          fprintf(stderr, "line %d: %s\n", i + 1, ln.c_str());
-            i ++;
-        }
+  }
+
+  {
+    char fn[] = "abc.txt.bgzip.gz";
+    FileWriter fw(fn, BGZIP);
+    fw.write(a);
+    fw.close();
+
+    LineReader lr(fn);
+    std::string ln;
+    int i = 0;
+    while (lr.readLine(&ln)) {
+      assert(0 == strcmp(t[i].c_str(), ln.c_str()));
+      i++;
     }
-    return 0;
+  }
+
+  {
+    char fn[] = "http://zhanxw.com/rvtests/version";
+    LineReader lr(fn);
+    std::string ln;
+    int i = 0;
+    while (lr.readLine(&ln)) {
+      fprintf(stderr, "line %d: %s\n", i + 1, ln.c_str());
+      i++;
+    }
+  }
+  {
+    char fn[] = "http://zhanxw.com/rvtests/version.gz";
+    LineReader lr(fn);
+    std::string ln;
+    int i = 0;
+    while (lr.readLine(&ln)) {
+      fprintf(stderr, "line %d: %s\n", i + 1, ln.c_str());
+      i++;
+    }
+  }
+  return 0;
 }

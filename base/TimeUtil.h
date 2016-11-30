68311a5b91b1fc100abfd7589851c52a7337d2cd
zhanxw <zhanxw@gmail.com>
2016-11-30 14:36:06 -0600

    inline a function

--- base/TimeUtil.h
+++ base/TimeUtil.h
@@ -7,7 +7,7 @@
 /**
  * @return a string representing current time, without '\n' at the end
  */
-std::string currentTime() {
+inline std::string currentTime() {
   time_t t = time(NULL);
   std::string s(ctime(&t));
   s = s.substr(0, s.size() - 1);

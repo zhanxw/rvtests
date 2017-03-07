4bf9ada7c5cabefe0e5151bcde464591e8b73d97
zhanxw <zhanxw@gmail.com>
2017-03-07 14:14:11 -0600

    update Makefile

---
+++ regression/test/testLinearVT.R
@@ -0,0 +1,40 @@
+outFile = commandArgs(trailingOnly = TRUE)[1]
+library(mvtnorm)
+source("~/mylib/R/linear.mvt.R")
+set.seed(0)
+N <- 1000
+M <- 2
+p <- seq(M) * 0.01
+
+x <- do.call(cbind, lapply(p, function(x){rbinom(N, 2, x)}))
+colMeans(x)
+cor.rand <- matrix(rnorm(3*3), 3)
+cor.rand <- t(cor.rand) %*% cor.rand
+covar <- rmvnorm(N, mean = rep(0, 3), sigma = cor.rand)
+covar <- cbind(rep(1, nrow(covar)), covar)
+#y <- covar %*% seq(ncol(covar)) + rnorm(N)
+y <- rnorm(N)
+
+summary(ret <- lm(y ~ covar - 1))
+colSums(x)
+
+y.res <- resid(ret)
+v <- sum(y.res ^ 2) / N
+
+S <- x
+S[,2] <- S[,1] + S[,2]
+x <- S
+
+ret <- linear.mvt(y.res, S, covar, v)
+
+out <- rbind(ret$U, ret$V, ret$t, ret$V.all, c(ret$p, 0))
+
+write.table(x,     "input.linear.mvt.x", quote = FALSE, row.names = FALSE, col.names=FALSE)
+write.table(y,     "input.linear.mvt.y", quote = FALSE, row.names = FALSE, col.names=FALSE)
+write.table(covar, "input.linear.mvt.cov", quote = FALSE, row.names = FALSE, col.names=FALSE)
+write.table(out, outFile, quote = FALSE, row.names = FALSE, col.names=FALSE)
+
+if (F){
+    l.res <-as.matrix(read.table("res"))
+    l.S <-  as.matrix(read.table("S"))
+}
---
+++ regression/test/testLogisticVT.R
@@ -0,0 +1,41 @@
+outFile = commandArgs(trailingOnly = TRUE)[1]
+
+source("~/mylib/R/logistic.mvt.R")
+set.seed(0)
+N <- 1000
+M <- 2
+p <- seq(M) * 0.01
+
+x <- do.call(cbind, lapply(p, function(x){rbinom(N, 2, x)}))
+colMeans(x)
+covar <- matrix(rnorm(N * 3), N, 3)
+covar <- cbind(rep(1, nrow(covar)), covar)
+y <- covar %*% seq(ncol(covar))
+y <- 1/(1+exp(-y))
+y <- rbinom(N, 1, y)
+
+summary(glm(y ~ x + covar - 1, family = "binomial"))
+colSums(x)
+
+y.null <- predict(glm(y ~ covar - 1, family = "binomial"), type = "response")
+summary(y.null)
+v <- y.null * (1-y.null)
+y.res <- y - y.null
+
+S <- x
+S[,2] <- S[,1] + S[,2]
+x <- S
+
+ret <- logistic.mvt(y.res, S, covar, v)
+
+out <- rbind(ret$U, ret$V, ret$t, ret$V.all, c(ret$p, 0))
+
+write.table(x,     "input.logistic.mvt.x", quote = FALSE, row.names = FALSE, col.names=FALSE)
+write.table(y,     "input.logistic.mvt.y", quote = FALSE, row.names = FALSE, col.names=FALSE)
+write.table(covar, "input.logistic.mvt.cov", quote = FALSE, row.names = FALSE, col.names=FALSE)
+write.table(out, outFile, quote = FALSE, row.names = FALSE, col.names=FALSE)
+
+if (F){
+    l.res <-as.matrix(read.table("res"))
+    l.S <-  as.matrix(read.table("S"))
+}

f8525c61c56cb366c864e209d0d8f76ba53578a5
zhanxw <zhanxw@gmail.com>
2017-10-09 16:27:54 -0500

    optimize BGenFile reading speed (B=8,16)

--- libBgen/BitReader.h
+++ libBgen/BitReader.h
@@ -1,6 +1,12 @@
 #ifndef _BITREADER_H_
 #define _BITREADER_H_
 
+/**
+ * B = 3
+ * ptr[0]:   22 111 000
+ * ptr[1]:   5 444 333 2
+ * ptr[2]:   777 666 55
+ */
 class BitReader {
  public:
   BitReader(uint8_t* ptr, int len, const int B)
@@ -22,6 +28,15 @@ class BitReader {
     return (ptr[byte] & (1 << remain)) != 0 ? 1 : 0;
   }
   float next() {
+    if (B == 8) {
+      offset += B;
+      return (float(ptr[offset / 8]) * scale);
+    }
+    if (B == 16) {
+      uint16_t v = *(uint16_t*)(ptr + (offset / 8));
+      offset += B;
+      return (float(v) * scale);
+    }
     int res = 0;
     for (int i = 0; i < B; ++i) {
       res |= getBit(i) << i;
@@ -32,10 +47,10 @@ class BitReader {
 
  private:
   uint8_t* ptr;
-  int offset;
+  int offset;  // 0-based index
   const int B;
   float scale;
-  int len;
+  int len;  // length of the ptr
 };
 
 #endif /* _BITREADER_H_ */

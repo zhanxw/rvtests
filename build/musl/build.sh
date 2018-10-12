b213ea8e2b6ca2d6be29eaca1d507cdc265f9b8e
zhanxw <zhanxw@gmail.com>
2018-10-12 00:26:46 -0500

    add musl

---
+++ build/musl/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -xue
+
+MXE_DIR=$HOME/x86_64-linux-musl-8.2.0
+export PATH=${MXE_DIR}/bin:$PATH
+
+MXE_TARGET=x86_64-linux-musl
+
+export CC=${MXE_DIR}/bin/${MXE_TARGET}-gcc
+export CXX=${MXE_DIR}/bin/${MXE_TARGET}-g++
+export LD=${MXE_DIR}/bin/${MXE_TARGET}-ld
+export AR=${MXE_DIR}/bin/${MXE_TARGET}-ar
+export FC=${MXE_DIR}/bin/${MXE_TARGET}-gfortran
+export PKG_CONFIG=${MXE_DIR}/bin/${MXE_TARGET}-pkg-config
+export LDFLAGS=
+
+make -f Makefile.win CROSS=${MXE_TARGET} MUSL=1
+
+set +xue
---
+++ build/musl/requirements.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -xue
+
+sudo apt-get update
+
+sudo apt-get --yes install \
+    g++ make cmake \
+    gfortran \
+    wget
+    #libqt4-dev upx-ucl \
+
+wget https://skarnet.org/toolchains/native/x86_64-linux-musl-8.2.0.tar.xz
+
+tar xf x86_64-linux-musl-8.2.0.tar.xz
+
+set +xue

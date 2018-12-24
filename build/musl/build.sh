e57ae817e42239b3045153aad6c89522db72edac
zhanxw <zhanxw@gmail.com>
2018-12-24 01:20:17 -0600

    fix musl

--- build/musl/build.sh
+++ build/musl/build.sh
@@ -7,10 +7,12 @@ MXE_TARGET=x86_64-linux-musl
 
 export CC=${MXE_DIR}/bin/${MXE_TARGET}-gcc
 export CXX=${MXE_DIR}/bin/${MXE_TARGET}-g++
-export LD=${MXE_DIR}/bin/${MXE_TARGET}-ld
-export AR=${MXE_DIR}/bin/${MXE_TARGET}-ar
-export FC=${MXE_DIR}/bin/${MXE_TARGET}-gfortran
-export PKG_CONFIG=${MXE_DIR}/bin/${MXE_TARGET}-pkg-config
+export LD=${MXE_DIR}/bin/ld
+#export LD=${MXE_DIR}/bin/${MXE_TARGET}-ld
+export AR=${MXE_DIR}/bin/${MXE_TARGET}-gcc-ar  #
+#export AR=${MXE_DIR}/bin/${MXE_TARGET}-ar
+#export FC=${MXE_DIR}/bin/${MXE_TARGET}-gfortran
+#export PKG_CONFIG=${MXE_DIR}/bin/${MXE_TARGET}-pkg-config
 export LDFLAGS=
 
 ls ${MXE_DIR}/bin

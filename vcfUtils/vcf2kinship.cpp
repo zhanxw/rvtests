33da04873581e195c6c3b540110a165c59bbc740
Chris X. Weichenberger <christian.weichenberger@eurac.edu>
2019-10-28 11:58:44 +0100

    Bump version number.
    Bugfix should also be recognizable by looking at the version number.

--- vcfUtils/vcf2kinship.cpp
+++ vcfUtils/vcf2kinship.cpp
@@ -439,7 +439,7 @@ int output(const std::vector<std::string>& famName,
            bool performPCA, const std::string& outPrefix);
 
 #define PROGRAM "vcf2kinship"
-#define VERSION "20170307"
+#define VERSION "20191018"
 void welcome() {
 #ifdef NDEBUG
   fprintf(stdout, "Thank you for using %s (version %s, git tag %s)\n", PROGRAM,
